215b4f3511e0c7465d9cfb58908ae55d94e397cf
Zuko <jose.apep570@gmail.com>
2023-05-20 09:58:08 -0600

    [ADD] First window with 'open file' button

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("First Commit")
+from tkinter import Tk, Button, Label, filedialog
+
+
+def open_files():
+    root = Tk()
+    root.withdraw()
+
+    file = filedialog.askopenfile()  # Open the dialog box
+    if file:
+        # Open file process
+        content = file.read()
+        print(content)
+        file.close()
+
+# Create the main window
+window = Tk()
+window.geometry("400x200")
+window.title("ProbGraph")
+window.eval('tk::PlaceWindow . center')  # Center the program
+
+# Create the "Open file" section
+text = Label(window, text="Seleccione el archivo '.csv' que desee analizar")
+text.pack(anchor="center", expand="true")
+open_button = Button(window, text="Abrir archivo", command=open_files)
+open_button.pack(anchor="center", expand="true")
+
+# Initiate the event loop for the graphical interface
+window.mainloop()
