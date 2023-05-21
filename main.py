20a36e09d4ef0ada37a6658c391b493f30d6766f
Zuko <jose.apep570@gmail.com>
2023-05-20 18:27:59 -0600

    [UPDATED] main and graphics

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Label, filedialog
+from tkinter import Tk, Button, Label, filedialog, ttk
 import pandas as pd
 
 def open_files():
@@ -8,22 +8,34 @@ def open_files():
     file = filedialog.askopenfile(filetypes=[('CSV Files', '*.csv')])  # Open the dialog box
     if file:
         # Open file process
-        content = pd.read_csv(file, delimiter=';')
-        attributes = content.columns[0].replace('"', '').split(';')
-        print(content)
-        file.close()
+        content = pd.read_csv(file)
+        attributes = content.columns.tolist()
+
+        Label(window, text="Seleccione el atributo y el tipo de gráfico que desea realizar").place(x=60, y=130)
+        combobox_attributes = ttk.Combobox(window, values=attributes)
+        combobox_attributes.place(x=60, y=150)
+
+        combobox_graph = ttk.Combobox(window, values=["Barras", "Pastel"])
+        combobox_graph.place(x=200, y=150)
+
+        def get_column_values():
+            selected_column = combobox_attributes.get()
+            column_values = content[selected_column].tolist()
+            Label(window, text=column_values).place(x=60, y=220)
+
+        values_button = Button(window, text="Obtener gráfica", command=get_column_values)
+        values_button.place(x=60, y=190)
 
 # Create the main window
 window = Tk()
-window.geometry("400x200")
+window.geometry("900x500")
 window.title("ProbGraph")
-window.eval('tk::PlaceWindow . center')  # Center the program
 
 # Create the "Open file" section
 text = Label(window, text="Seleccione el archivo '.csv' que desee analizar")
-text.pack(anchor="center", expand="true")
+text.place(x=60, y=40)
 open_button = Button(window, text="Abrir archivo", command=open_files)
-open_button.pack(anchor="center", expand="true")
+open_button.place(x=60, y=60)
 
 # Initiate the event loop for the graphical interface
 window.mainloop()
---
+++ services/graphics.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+null = 0
+def histogram_plot(data):
+    return null
+
+def frequency_polygon_graph(data):
+    return null
+
+def warhead_graph(data):
+    return null
+
+def bar_graph(data):
+    return null
+
+def pie_chart(data):
+    pd.value_counts().
+    return null
